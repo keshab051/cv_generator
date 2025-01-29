5199372af56b6bb56cc63b66c6497495e91bcf54
KeshabNeupane <keshabneupane061@gmail.com>
2025-01-29 07:52:12 +0545

    object converted into self.name

--- pdf/models.py
+++ pdf/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class profile(models.Model):
+    def __str__(self):
+        return self.name;
+
     name = models.CharField(max_length=2000)
     email = models.CharField(max_length=2000)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=200)
     summary = models.TextField(max_length=2000)
     degree = models.CharField(max_length=200)
     school = models.CharField(max_length=200)
