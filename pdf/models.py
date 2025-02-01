bfbb7e59dd3ee0382df6e8810d649b3dd302ce2b
KeshabNeupane <keshabneupane061@gmail.com>
2025-02-01 10:36:11 +0545

    cv data get by using form

--- pdf/models.py
+++ pdf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     def __str__(self):
-        return self.name;
+        return self.name
 
     name = models.CharField(max_length=2000)
     email = models.CharField(max_length=2000)
--- pdf/views.py
+++ pdf/views.py
@@ -1,20 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Profile
+from .forms import ProfileForm
 
-def accept(request):
-    if request.method =="POST":
-        name = request.POST.get("name","")
-        email = request.POST.get("email","")
-        phone = request.POST.get("phone","")
-        summary = request.POST.get("summary","")
-        degree = request.POST.get("degree","")
-        school = request.POST.get("school","")
-        university = request.POST.get("university","")
-        previous_work = request.POST.get("previous_work","")
-        skills = request.POST.get("skills","")
+# def accept(request):
+#     form= movie_list(request.POST)
+#     form.save()
 
-        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,university=university,previous_work=previous_work,skills=skills)
-        profile.save()
-    return render(request,'pdf/accept.html')
+#     # if request.method =="POST":
+#     #     name = request.POST.get("name","")
+#     #     email = request.POST.get("email","")
+#     #     phone = request.POST.get("phone","")
+#     #     summary = request.POST.get("summary","")
+#     #     degree = request.POST.get("degree","")
+#     #     school = request.POST.get("school","")
+#     #     university = request.POST.get("university","")
+#     #     previous_work = request.POST.get("previous_work","")
+#     #     skills = request.POST.get("skills","")
+
+#     #     profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,university=university,previous_work=previous_work,skills=skills)
+#     #     profile.save()
+#     return render(request,'pdf/accept.html')
 
 # Create your views here.
+
+
+def accept(request):      
+    form = ProfileForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('accept')  
+    else:
+        form = ProfileForm()
+    return render(request, 'pdf/accept.html', {'form': form})
